2f6ccfa4b9143c92de3f1d77ea0f86e5e1b20140
OkaGouhei <g.oka1975@gmail.com>
2019-03-05 22:54:57 +0900

    修正
    ・コメント
    ・メソッドの頭にはjavadocをつけて説明

--- BlackJack.java
+++ BlackJack.java
@@ -18,12 +18,18 @@ public class BlackJack{
       else {
         System.out.println("もう一度お願いします");
       }
-    }while(draw != 1);
+    }while(draw != 1);/* 1 を選択しない限り繰り返します
   }
+  /**
+  * dealメソッド
+  * トランプ1セット（52枚)からランダムに1枚提示する
+  * @param number 1-13 までが格納される
+  * @param mark_num 0-3までが格納される
+  */
   private static void deal(){
     Random rnd1 = new Random();
     int ran1 = rnd1.nextInt(52);
-    int number = ran1 % 13;
+    int number = ran1 % 13 + 1;
     int mark_num = ran1 % 4;
     String mark[] = {"ハート", "クラブ", "ダイヤ","スペード"};
     System.out.println(mark[mark_num ]+":"+number);
